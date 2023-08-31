0d09a65f1fb76ca00e7ed581f963ad0d31500403
iewouk1004 <a102222071@gmail.com>
2023-08-31 15:04:13 +0800

    修改主頁

--- app/page.tsx
+++ app/page.tsx
@@ -8,30 +8,31 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+        デジモンアドベンチャー<br className="hidden sm:inline" />
+        Digmon Adventure <br className="hidden sm:inline" />
+        數碼寶貝大冒險
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
+        想成為數碼寶貝訓練家嗎
         </p>
       </div>
       <div className="flex gap-4">
         <Link
-          href={siteConfig.links.docs}
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noreferrer"
           className={buttonVariants()}
         >
-          Documentation
+          點我看更多
         </Link>
+
         <Link
           target="_blank"
           rel="noreferrer"
-          href={siteConfig.links.github}
+          href="https://zh.wikipedia.org/zh-tw/%E6%95%B0%E7%A0%81%E5%AE%9D%E8%B4%9D%E7%B3%BB%E5%88%97"
           className={buttonVariants({ variant: "outline" })}
         >
-          GitHub
+          wikipedia
         </Link>
       </div>
     </section>
